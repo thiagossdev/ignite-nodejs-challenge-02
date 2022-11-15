bf911fb5c6959ee8ffcd27ece02f99c81c90388e
Thiago S. Santos <ssthiago.dev@gmail.com>
2022-11-15 20:07:48 -0300

    Add check user plan

--- src/index.js
+++ src/index.js
@@ -25,7 +25,15 @@ function checksExistsUserAccount(request, response, next) {
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (!user.pro && user.todos.length >= 10) {
+    return response.status(403).json({
+      error: 'User unauthorized!',
+    });
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
