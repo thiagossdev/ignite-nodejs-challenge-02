e6d9be3e14acae9e903dee094bc5d5aac8ef7f94
Thiago S. Santos <ssthiago.dev@gmail.com>
2022-11-15 20:07:16 -0300

    Add check username in header

--- src/index.js
+++ src/index.js
@@ -10,7 +10,18 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+  request.user = user;
+
+  if (!user) {
+    return response.status(404).json({
+      error: 'User not found!',
+    });
+  }
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
